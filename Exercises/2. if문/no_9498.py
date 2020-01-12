47e9cd9d2b941d066505efe3a783ad5cb0e7c98e
drhee0919 <drhee0919@naver.com>
2020-01-12 11:33:46 +0900

    directory update(20/01/12)

---
+++ Exercises/2. if문/no_9498.py
@@ -0,0 +1,12 @@
+A = map(int, input())
+if 100>=A>=90 :
+    print("A")
+elif 89>=A>=80:
+    print("B")
+elif 100>=A>=90:
+    print("C")    
+elif 100>=A>=90:
+    print("D") 
+else:
+    print("F")
+    
