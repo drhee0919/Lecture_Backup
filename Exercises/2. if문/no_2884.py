f6a79d3fb4d15cf854a016143baac19445fb3a82
drhee0919 <drhee0919@naver.com>
2020-01-26 02:36:07 +0900

    directory update(20/01/26)

--- Exercises/2. if문/no_2884.py
+++ Exercises/2. if문/no_2884.py
@@ -1,6 +1,7 @@
-import datetime
-from datetime import time  
-H, M = map(int, input().split())
-input_time = datetime(H, M)
-output_time = input_time - datetime.timedelta(minutes=45)
-print(output_time)
+H,M = map(int, input().split())
+if M > 44:
+    print(H, M-45)
+elif M <= 44 and H >= 1:
+    print(H-1, M+15)
+else:
+    print(23, M+15)
